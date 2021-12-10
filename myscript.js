23e5fec6399a652c14546b405f5a053633ac719c
AntonyMorgante <antonymorgante1@gmail.com>
2021-12-10 16:42:54 +0100

    script refined

--- myscript.js
+++ myscript.js
@@ -1,30 +1,23 @@
 const msCont = document.getElementById("container");
 
 for (i = 1; i <= 100; i++){
+    var msCommon = document.createElement("msCommon")
+    msCommon.classList.add("square");
     if ((i % 3 == 0) && (i % 5 == 0)) {
-        var msCommon = document.createElement("msCommon")
         msCommon.innerHTML= "FizzBuzz";
-        msCommon.classList.add("square");
         msCommon.classList.add("blue");
-        msCont.appendChild(msCommon);     }
+    }
     else if (((i % 3) == 0) && ((i % 5) != 0)){
-        var msCommon = document.createElement("msCommon")
         msCommon.innerHTML= "Fizz";
-        msCommon.classList.add("square");
         msCommon.classList.add("orange");
-        msCont.appendChild(msCommon);    }
+    }
     else if (((i % 3) != 0) && ((i % 5) == 0)){
-        var msCommon = document.createElement("msCommon")
         msCommon.innerHTML= "Buzz";
-        msCommon.classList.add("square");
         msCommon.classList.add("green");
-        msCont.appendChild(msCommon);
     }
     else {
-        var msCommon = document.createElement("msCommon")
         msCommon.innerHTML= i;
-        msCommon.classList.add("square");
         msCommon.classList.add("gray");
-        msCont.appendChild(msCommon);
     }
+    msCont.appendChild(msCommon); 
 }       
